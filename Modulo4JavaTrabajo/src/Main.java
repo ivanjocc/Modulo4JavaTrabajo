ffbc54b9eee451a2f22f8609cc12d63893525230
Ivan <ivanjose_04@hotmail.com>
2023-05-12 17:38:50 -0500

    first commit

---
+++ Modulo4JavaTrabajo/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        String [] nombres = {"atunes","prueba","test","el tigro"};
+        String resultado = "";
+
+        for (int i = 0; i < nombres.length; i++) {
+            resultado += nombres[i];
+        }
+
+        System.out.println(resultado);
+    }
+}
